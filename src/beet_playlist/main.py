46a441d09d40ef28d755be4414ac1f2cdfac4d5e
Ignacio Rossi <rossi.ignacio@gmail.com>
2014-03-16 17:09:44 -0300

    Move common stuff to its own module

--- src/beet_playlist/main.py
+++ src/beet_playlist/main.py
@@ -3,9 +3,8 @@ import logging
 import os
 import sys
 import subprocess
-
-PLAYLISTS_DIR = "/home/ignacio/Music/.playlists"
-PLAYLISTS_DATA_FILE = ".playlists_data"
+from beet_playlist.common import PLAYLISTS_DIR, is_playlist, PLAYLISTS_DATA_FILE, \
+    get_playlist_contents, check_playlist, get_playlist_dir
 
 def _get_arg_parser():
     parser = ArgumentParser()
@@ -31,29 +30,12 @@ def _init_playlists_dir():
     if not os.path.isdir(PLAYLISTS_DIR):
         os.makedirs(PLAYLISTS_DIR)
 
-def _get_playlist_dir(playlist):
-    return os.path.join(PLAYLISTS_DIR, playlist)
-
-def _get_playlist_contents(playlist):
-    return sorted(os.listdir(_get_playlist_dir(playlist)))
-
-def _is_playlist(playlist):
-    return os.path.isdir(os.path.join(PLAYLISTS_DIR, playlist))
-
-def _check_playlist(playlist):
-    if playlist is None:
-        logging.error("Must supply a playlist name")
-        sys.exit(1)
-    elif not _is_playlist(playlist):
-        logging.error("Invalid playlist '%s'", playlist)
-        sys.exit(1)
-
 def new(playlist):
     print "new(%s)" % playlist
     if playlist is None:
         logging.error("Must supply a playlist name")
         sys.exit(1)
-    elif _is_playlist(playlist):
+    elif is_playlist(playlist):
         logging.error("Playlist '%s' already exsts", playlist)
         sys.exit(1)
     else:
@@ -64,16 +46,16 @@ def list_(playlist):
     if playlist is None:
         for playlist in sorted(os.listdir(PLAYLISTS_DIR)):
             if playlist != PLAYLISTS_DATA_FILE:
-                print "%s - %d tracks" % (playlist, len(_get_playlist_contents(playlist)))
+                print "%s - %d tracks" % (playlist, len(get_playlist_contents(playlist)))
     else:
-        _check_playlist(playlist)
-        print "Playlist '%s' - %d tracks" % (playlist, len(_get_playlist_contents(playlist)))
-        for track in _get_playlist_contents(playlist):
+        check_playlist(playlist)
+        print "Playlist '%s' - %d tracks" % (playlist, len(get_playlist_contents(playlist)))
+        for track in get_playlist_contents(playlist):
             print " - %s" % track
 
 def add(playlist, query):
     print "add(%s), query: '%s'" % (playlist, query)
-    _check_playlist(playlist)
+    check_playlist(playlist)
     beet_format = '$artist\t$album\t$title\t$path'
     proc = subprocess.Popen(['beet', 'list', '-f', beet_format] + query, stdout=subprocess.PIPE)
     lines = proc.stdout.readlines()
@@ -89,18 +71,18 @@ def add(playlist, query):
 
     artist, album, title, path = lines[0].rstrip("\n").split("\t")
     print "Matched: %s - %s - %s" % (artist, album, title)
-    size = len(_get_playlist_contents(playlist))
+    size = len(get_playlist_contents(playlist))
     fname_bits = ["%06d" % (size + 1), artist, album, title]
     fname = "-".join(fname_bit.replace(" ", "_") for fname_bit in fname_bits)
-    dest = os.path.join(_get_playlist_dir(playlist), fname)
+    dest = os.path.join(get_playlist_dir(playlist), fname)
     os.link(path, dest)
 
 
 def play(playlist):
     print "play(%s)" % playlist
-    _check_playlist(playlist)
-    playlist_dir = _get_playlist_dir(playlist)
-    contents = [os.path.join(playlist_dir, track) for track in _get_playlist_contents(playlist)]
+    check_playlist(playlist)
+    playlist_dir = get_playlist_dir(playlist)
+    contents = [os.path.join(playlist_dir, track) for track in get_playlist_contents(playlist)]
     subprocess.call(["mplayer"] + contents)
 
 
