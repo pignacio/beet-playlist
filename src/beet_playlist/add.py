296a16e2be7406455dd572eb1aa4caa40622f650
Ignacio Rossi <rossi.ignacio@gmail.com>
2014-03-16 17:16:49 -0300

    Encapsulate tracks in objects

--- src/beet_playlist/add.py
+++ src/beet_playlist/add.py
@@ -10,26 +10,43 @@ import os
 import subprocess
 import sys
 
-def add(playlist, query):
-    print "add(%s), query: '%s'" % (playlist, query)
-    check_playlist(playlist)
+class BeetTrack():
+    def __init__(self, artist, album, title, path):
+        self.artist = artist
+        self.album = album
+        self.title = title
+        self.path = path
+
+    def __str__(self):
+        return " - ".join([self.artist, self.album, self.title])
+
+
+def _run_beet_query(query):
     beet_format = '$artist\t$album\t$title\t$path'
     proc = subprocess.Popen(['beet', 'list', '-f', beet_format] + query, stdout=subprocess.PIPE)
     lines = proc.stdout.readlines()
     proc.wait()
-    if len(lines) == 0:
+    return [BeetTrack(*line.rstrip('\n').split("\t")) for line in lines]
+
+def add(playlist, query):
+    print "add(%s), query: '%s'" % (playlist, query)
+    check_playlist(playlist)
+
+    tracks = _run_beet_query(query)
+
+    if len(tracks) == 0:
         logging.error("No tracks matched")
         sys.exit(1)
-    elif len(lines) > 1:
-        for line in lines:
-            print line.rstrip("\n")
+    elif len(tracks) > 1:
+        for track in tracks:
+            print track
         logging.error("More than one track matched")
         sys.exit(1)
 
-    artist, album, title, path = lines[0].rstrip("\n").split("\t")
-    print "Matched: %s - %s - %s" % (artist, album, title)
+    track = tracks[0]
+    print "Matched: %s" % track
     size = len(get_playlist_contents(playlist))
-    fname_bits = ["%06d" % (size + 1), artist, album, title]
+    fname_bits = ["%06d" % (size + 1), track.artist, track.album, track.title]
     fname = "-".join(fname_bit.replace(" ", "_") for fname_bit in fname_bits)
     dest = os.path.join(get_playlist_dir(playlist), fname)
-    os.link(path, dest)
+    os.link(track.path, dest)
