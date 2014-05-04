63d61fa82404e0cdc6d58e485ec56db09367e979
Ignacio Rossi <rossi.ignacio@gmail.com>
2014-05-04 14:23:18 -0300

    Using subparsers

--- src/beet_playlist/main.py
+++ src/beet_playlist/main.py
@@ -1,85 +1,40 @@
 from argparse import ArgumentParser
-from add import add
-from common import PLAYLISTS_DIR, is_playlist, PLAYLISTS_DATA_FILE, \
-    get_playlist_contents, check_playlist, get_playlist_dir
+from common import add_parsers, PLAYLISTS_DIR
+import add
 import logging
 import os
-import subprocess
 import sys
 
 def _get_arg_parser():
     parser = ArgumentParser()
-    parser.add_argument("-n" , "--new",
-                        action='store_true', default=False,
-                        help='Adds a new playlist')
-    parser.add_argument("-l" , "--list",
-                        action='store_true', default=False,
-                        help='Lists current playlists')
-    parser.add_argument("-a" , "--add",
-                        action='store_true', default=False,
-                        help='Adds track to a playlist')
-    parser.add_argument("-p" , "--play",
-                        action='store_true', default=False,
-                        help='Plays a playlist')
-    parser.add_argument('playlist', nargs="?", default=None,
-                        help='playlist to act on')
-    parser.add_argument('query', nargs="*",
-                        help='query to act on')
+    subparsers = parser.add_subparsers(dest='subparser')
+    add_parsers(subparsers)
+    add.add_parser(subparsers)
     return parser
 
 def _init_playlists_dir():
     if not os.path.isdir(PLAYLISTS_DIR):
         os.makedirs(PLAYLISTS_DIR)
 
-def new(playlist):
-    print "new(%s)" % playlist
-    if playlist is None:
-        logging.error("Must supply a playlist name")
-        sys.exit(1)
-    elif is_playlist(playlist):
-        logging.error("Playlist '%s' already exsts", playlist)
-        sys.exit(1)
-    else:
-        os.makedirs(os.path.join(PLAYLISTS_DIR, playlist))
-
-def list_(playlist):
-    print "list(%s)" % playlist
-    if playlist is None:
-        for playlist in sorted(os.listdir(PLAYLISTS_DIR)):
-            if playlist != PLAYLISTS_DATA_FILE:
-                print "%s - %d tracks" % (playlist, len(get_playlist_contents(playlist)))
-    else:
-        check_playlist(playlist)
-        print "Playlist '%s' - %d tracks" % (playlist, len(get_playlist_contents(playlist)))
-        for track in get_playlist_contents(playlist):
-            print " - %s" % track
-
-def play(playlist):
-    print "play(%s)" % playlist
-    check_playlist(playlist)
-    playlist_dir = get_playlist_dir(playlist)
-    contents = [os.path.join(playlist_dir, track) for track in get_playlist_contents(playlist)]
-    subprocess.call(["mplayer"] + contents)
-
+def _extract_from_options(key, options):
+    res = getattr(options, key)
+    delattr(options, key)
+    return res
 
 def main():
+    logging.basicConfig(stream=sys.stdout, level=logging.INFO)
     parser = _get_arg_parser()
     options = parser.parse_args()
-    if not any([options.new, options.list, options.add, options.play]):
-        print >> sys.stderr, "No action supplied"
-        parser.print_help()
-        sys.exit(1)
 
+    subparser = _extract_from_options("subparser", options)
+
+    try:
+        func = _extract_from_options("func", options)
+    except AttributeError:
+        raise Exception("Missing 'func' attribute. Maybe subparser '{}' didnt correctly set_defaults()?".format(subparser))
     _init_playlists_dir()
+    func(**options.__dict__)
 
-    if options.new:
-        new(options.playlist)
-    elif options.list:
-        list_(options.playlist)
-    elif options.add:
-        add(options.playlist, options.query)
-    elif options.play:
-        play(options.playlist)
 
 if __name__ == "__main__":
     main()
