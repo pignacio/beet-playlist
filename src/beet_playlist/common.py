1326010261f89c531a90da2009d37c631144668a
Ignacio Rossi <rossi.ignacio@gmail.com>
2014-05-04 14:23:29 -0300

    Removed unused functions

--- src/beet_playlist/common.py
+++ src/beet_playlist/common.py
@@ -3,7 +3,6 @@ Created on Mar 16, 2014
 
 @author: ignacio
 '''
-import sys
 import logging
 import os
 import subprocess
@@ -11,22 +10,6 @@ import subprocess
 PLAYLISTS_DIR = "/home/ignacio/Music/.playlists"
 PLAYLISTS_DATA_FILE = ".playlists_data"
 
-def get_playlist_dir(playlist):
-    return os.path.join(PLAYLISTS_DIR, playlist)
-
-def get_playlist_contents(playlist):
-    return sorted(os.listdir(get_playlist_dir(playlist)))
-
-def is_playlist(playlist):
-    return os.path.isdir(os.path.join(PLAYLISTS_DIR, playlist))
-
-def check_playlist(playlist):
-    if playlist is None:
-        logging.error("Must supply a playlist name")
-        sys.exit(1)
-    elif not is_playlist(playlist):
-        logging.error("Invalid playlist '%s'", playlist)
-        sys.exit(1)
 
 class Playlist():
     def __init__(self, playlist):
