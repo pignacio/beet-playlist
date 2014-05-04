2ba32488e3461e32e843bc5c8a9bfb7041b497c9
Ignacio Rossi <rossi.ignacio@gmail.com>
2014-05-04 17:31:50 -0300

    Added play_query command

--- src/beet_playlist/main.py
+++ src/beet_playlist/main.py
@@ -4,12 +4,15 @@ import add
 import logging
 import os
 import sys
+from beets_api import run_beet_query
+import subprocess
 
 def _get_arg_parser():
     parser = ArgumentParser()
     subparsers = parser.add_subparsers(dest='subparser')
     common.add_parsers(subparsers)
     add.add_parser(subparsers)
+    add_parsers(subparsers)
     return parser
 
 def _init_playlists_dir():
@@ -35,6 +38,20 @@ def main():
     _init_playlists_dir()
     func(**options.__dict__)
 
+
+def add_parsers(subparsers):
+    parser = subparsers.add_parser("play_query")
+    parser.add_argument('query', nargs="*",
+                         help='beets query to play')
+    parser.set_defaults(func=play_beets_query)
+
+def play_beets_query(query):
+    tracks = run_beet_query(query)
+    logging.info("Got %s tracks from query '%s'", len(tracks), query)
+    for track in tracks:
+        logging.info(" - %s", track.path)
+    command = ['mplayer'] + [t.path for t in tracks]
+    subprocess.call(command)
 
 if __name__ == "__main__":
     main()
